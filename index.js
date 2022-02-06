aada339efe785a2c243e763060eef8027ebc4cf6
Angelo <angelomilienjoseph@gmail.com>
2022-02-06 02:30:08 -0500

    commit tree 2b04bf177db228b77ff647e25e155e3bf78ac751

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+class Polygon {
+
+
+    get countSides(){
+       
+        return 3;
+    }
+
+    get perimeter(){
+        return 15
+    }
+}
+
+class Triangle {
+
+
+    // get countSides(){
+       
+    //     return 3;
+    // }
+
+    // get perimeter(){
+    //     return 15
+    // }
+}
